1676989f9cb5fb6a2dd8c6c630ad211f0ef33884
Adriana <adriannalmeida0@gmail.com>
2024-03-05 14:09:57 +0000

    cities class

---
+++ Cities.cpp
@@ -0,0 +1,5 @@
+//
+// Created by adriana on 3/5/24.
+//
+
+#include "Cities.h"
---
+++ Cities.h
@@ -0,0 +1,14 @@
+//
+// Created by adriana on 3/5/24.
+//
+
+#ifndef PROJETO1_CITIES_H
+#define PROJETO1_CITIES_H
+
+
+class Cities {
+
+};
+
+
+#endif //PROJETO1_CITIES_H
