f35fcc11b56443a79e4e2d38139a9060d1335578
SarinhaCortez <up202205636@g.uporto.pt>
2024-04-03 14:38:18 +0100

    main updated

--- main.cpp
+++ main.cpp
@@ -5,29 +5,27 @@
 #include "menu.h"
 
 int main() {
-    unordered_map<string, Reservoir> reservoirs_code;
+     unordered_map<string, Reservoir> reservoirs_code;
     unordered_map<string, City> cities_code;
     unordered_map<string, Station> stations_code;
     set<string> r_set, s_set, c_set;
     Graph<string> g;
-
-
+/*
     string path_reser = "../Project1LargeDataSet/Project1LargeDataSet/Reservoir.csv";
     string path_station = "../Project1LargeDataSet/Project1LargeDataSet/Stations.csv";
     string path_cities = "../Project1LargeDataSet/Project1LargeDataSet/Cities.csv";
     string path_pipes = "../Project1LargeDataSet/Project1LargeDataSet/Pipes.csv";
+    */
+    string path_reser = "../Project1DataSetSmall/Project1DataSetSmall/Reservoirs_Madeira.csv";
+    string path_station = "../Project1DataSetSmall/Project1DataSetSmall/Stations_Madeira.csv";
+    string path_cities = "../Project1DataSetSmall/Project1DataSetSmall/Cities_Madeira.csv";
+    string path_pipes = "../Project1DataSetSmall/Project1DataSetSmall/Pipes_Madeira.csv";
 
-   /* string path_reser = "/home/adriana/2leic/2S/DA/projeto1/Project1DataSetSmall/Project1DataSetSmall/Reservoirs_Madeira.csv";
-    string path_station = "/home/adriana/2leic/2S/DA/projeto1/Project1DataSetSmall/Project1DataSetSmall/Stations_Madeira.csv";
-    string path_cities = "/home/adriana/2leic/2S/DA/projeto1/Project1DataSetSmall/Project1DataSetSmall/Cities_Madeira.csv";
-    string path_pipes = "/home/adriana/2leic/2S/DA/projeto1/Project1DataSetSmall/Project1DataSetSmall/Pipes_Madeira.csv";*/
-
-    ReservoirParser(path_reser, g, reservoirs_code, r_set);
-    CityParser(path_cities, g, cities_code, c_set);
-
-    StationParser(path_station, g, stations_code, s_set);
+    ReservoirParser(path_reser, g, reservoirs_code);
+    CityParser(path_cities, g, cities_code);
+    StationParser(path_station, g, stations_code);
     PipesParser(path_pipes, g);
-
+    
     //mainMenu(g, cities_code, reservoirs_code);
     /*for(auto x : g.getVertexSet()){
         auto info = x->getInfo();
