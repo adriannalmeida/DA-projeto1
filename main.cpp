ffcdb8ddaffcba25c9e7da5abafd0cb2b83342a5
Marta Silva <up202208258@fe.up.pt>
2024-04-03 16:13:14 +0100

    Max flow de acordo com Gonçalo

--- main.cpp
+++ main.cpp
@@ -10,17 +10,17 @@ int main() {
     unordered_map<string, Station> stations_code;
     set<string> r_set, s_set, c_set;
     Graph<string> g;
-/*
+
     string path_reser = "../Project1LargeDataSet/Project1LargeDataSet/Reservoir.csv";
     string path_station = "../Project1LargeDataSet/Project1LargeDataSet/Stations.csv";
     string path_cities = "../Project1LargeDataSet/Project1LargeDataSet/Cities.csv";
     string path_pipes = "../Project1LargeDataSet/Project1LargeDataSet/Pipes.csv";
-    */
-    string path_reser = "../Project1DataSetSmall/Project1DataSetSmall/Reservoirs_Madeira.csv";
+
+    /*string path_reser = "../Project1DataSetSmall/Project1DataSetSmall/Reservoirs_Madeira.csv";
     string path_station = "../Project1DataSetSmall/Project1DataSetSmall/Stations_Madeira.csv";
     string path_cities = "../Project1DataSetSmall/Project1DataSetSmall/Cities_Madeira.csv";
     string path_pipes = "../Project1DataSetSmall/Project1DataSetSmall/Pipes_Madeira.csv";
-
+*/
 
     ReservoirParser(path_reser, g, reservoirs_code);
     CityParser(path_cities, g, cities_code);
@@ -71,7 +71,7 @@ int main() {
 /*
     cout << "Not supplied cities: "<< calculateReceivedSupply(g,cities_code) << endl;
     printNotFullySuppliedCities(g, cities_code);*/
-    chooseFailingReservoir(g, "R_1", reservoirs_code, cities_code);
+    //chooseFailingReservoir(g, "R_1", reservoirs_code, cities_code);
     //maxFlow
     //maxFlow(g, reservoirs_code);
 
@@ -91,6 +91,7 @@ int main() {
         cout<< "city: "<<cities_code[p.first].getCity()<<" demand ----->"<<cities_code[p.first].getDemand()<<"flow ------>"<< p.second<<endl;
     }*/
 
-    Balance(g, reservoirs_code, cities_code, stations_code);
+    //Balance(g, reservoirs_code, cities_code, stations_code);
+    chooseCityByName(g, reservoirs_code, cities_code, "none");
 }
 
